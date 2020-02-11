e6049e5726b66ad4a02701c5df42a7bab2635775
jaal5 <45917686+jaal5@users.noreply.github.com>
2020-02-10 20:44:06 -0500

    test

---
+++ overviewR.R
@@ -0,0 +1,27 @@
+# set up environment
+rm(list = ls())
+if(!is.null(dev.list())) dev.off()
+
+library(corrplot)
+library(dplyr)
+library(ggplot2)
+library(corrgram)
+
+# load and view data
+path_personal <- "C:\\Users\\janal\\OneDrive\\Dokumente\\GitHub\\"
+path_shared <- "cs699_project_classification\\data\\redfin_2020-02-10-15-33-53.csv"
+file <- paste0(path_personal,path_shared)
+data <- read.csv(file, header = TRUE)
+
+
+View(data)
+colnames(data)
+
+# get relevant variables
+data.rel <- na.omit(data[c(8,9,10,12,13,14,15,17,26,27)])
+
+# create overview of new dataset
+str(data.rel)
+cor(data.rel)
+corrgram(data.rel)
+pairs(data.rel)
